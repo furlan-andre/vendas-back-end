b3a34b2a245305030fd7def152140b6a84d8fed3
André Furlan <furlan.andre91@gmail.com>
2023-08-29 16:11:13 -0400

    Adicionado tag para swagger

--- src/Vendas/vendas.controller.ts
+++ src/Vendas/vendas.controller.ts
@@ -2,12 +2,22 @@ import { ProdutoService } from './../Produto/produto.service';
 import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, ClassSerializerInterceptor, HttpCode } from '@nestjs/common';
 import { VendasService } from './vendas.service';
 import { CreateVendaDto } from './dto/create-venda.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags("Venda")
 @Controller('Venda')
 export class VendasController {
   constructor(private readonly vendasService: VendasService,
               private readonly produtoService: ProdutoService) {}
 
+  
+  /// <summary>
+  ///   Cria uma nova venda.
+  /// </summary>
+  /// <param name="descricao">descrição da venda</param>
+  /// <param name="itens">listagem dos itens da vend</param>
+  /// <param name="cliente">cliente vinculado a vend</param>  
+  /// <returns>Status OK</returns>
   @Post()
   @HttpCode(201)
   criar(@Body() createVendaDto: CreateVendaDto) {
@@ -25,3 +35,4 @@ export class VendasController {
     return this.vendasService.obterPorId(+id);
   }
 }
+
