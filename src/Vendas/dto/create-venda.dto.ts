8b7b83cba2873868132726ad766f8009fc5d2523
André Furlan <furlan.andre91@gmail.com>
2023-08-28 21:29:56 -0400

    ajuste de validações

--- src/Vendas/dto/create-venda.dto.ts
+++ src/Vendas/dto/create-venda.dto.ts
@@ -4,16 +4,16 @@ import { ClienteDto } from "src/Cliente/dto/clientes.dto";
 import { VendaItemDto } from "src/VendaItem/dto/venda-item.dto";
 
 export class CreateVendaDto {        
-    @IsString({message: "A descricao deve ser informada (string)"})
+    // @IsString({message: "A descricao deve ser informada (string)"})
     descricao: string;
     
-    @IsNotEmpty({message: "Os itens devem ser informados"})
-    @ValidateNested({ each:true}) 
-    @Type(() => VendaItemDto)   
+    // @IsNotEmpty({message: "Os itens devem ser informados"})
+    // @ValidateNested({ each:true}) 
+    // @Type(() => VendaItemDto)   
     itens: VendaItemDto[]
 
-    @IsNotEmpty({message: "O cliente deve ser informado"})    
-    @ValidateNested({ each:true}) 
-    @Type(() => ClienteDto)   
+    // @IsNotEmpty({message: "O cliente deve ser informado"})    
+    // @ValidateNested({ each:true}) 
+    // @Type(() => ClienteDto)   
     cliente: ClienteDto
 }
